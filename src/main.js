b469b13458d420d61d57f88d16669b482ddf75c5
Vlad <mainrub06@gmail.com>
2020-01-15 20:18:24 +0300

    #12

--- src/main.js
+++ src/main.js
@@ -1,61 +1,43 @@
+import API from "./data";
 import search from "./components/search.js";
 import account from "./components/account.js";
 import menu from "./components/menu.js";
 import sort from "./components/sort.js";
-// import films from "./components/films.js";
-import card from "./components/card.js";
 
-import {films} from "./data";
-import {parseDuration, createElement} from "./utils";
-
-const COUNT_FILMS = 5;
-const COUNT_FILMS_TOP = 2;
-const COUNT_FILMS_MOST = 2;
+const AUTHORIZATION = `Basic er883jdzbdw=${Math.random()}`;
+const END_POINT = `https://htmlacademy-es-9.appspot.com/cinemaddict/`;
+const api = new API({
+  endPoint: END_POINT,
+  authorization: AUTHORIZATION
+});
 
 const doc = document;
 const $header = doc.querySelector(`.header`);
 const $main = doc.querySelector(`.main`);
+const $films = doc.querySelector(`.films`);
 
-
-const $films = doc.querySelector(`.films-list .films-list__container`);
-const $filmsTop = doc.querySelectorAll(`.films-list--extra .films-list__container`)[0];
-const $filmsMost = doc.querySelectorAll(`.films-list--extra .films-list__container`)[1];
-
-const renderHTML = function (inner, target, pos) {
+const renderBefore = function (inner, target) {
   const element = document.createElement(`template`);
   element.innerHTML = inner();
-  if(pos=== 'start') {
-    target.prepend(element.content);
-  } else {
-    target.append(element.content);
-  }
+  target.insertBefore(element.content, $films);
+};
+const renderAppend = function (inner, target) {
+  const element = document.createElement(`template`);
+  element.innerHTML = inner();
+  target.appendChild(element.content);
 };
 
 const renderComponents = () => {
-  renderHTML(search, $header);
-  renderHTML(account, $header);
-  renderHTML(menu, $main, 'start');
-  renderHTML(sort, $main, 'start');
-  // renderHTML(films, $main);
+  renderAppend(search, $header);
+  renderAppend(account, $header);
+  renderBefore(menu, $main);
+  renderBefore(sort, $main);
 
-  for (let i = 0; i < COUNT_FILMS; i++) {
-    let el = card(films[i]);
-    $films.append(createElement(el));
-  }
-  for (let i = 0; i < COUNT_FILMS_TOP; i++) {
-    let el = card(films[i]);
-    $filmsTop.append(createElement(el));
-  }
-  for (let i = 0; i < COUNT_FILMS_MOST; i++) {
-    let el = card(films[i]);
-    $filmsMost.append(createElement(el));
-  }
-
-
-
-
+  api.getMovies();
+  api.getMoviesExtraLeft();
+  api.getMoviesExtraRight();
 };
 
 
 renderComponents();
-
+api.getComments(5);
